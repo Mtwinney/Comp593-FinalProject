9c4b48b6b237b75b7f8211d6fece16d3981a4ab5
Mtwinney <mtwinney@flemingcollege.ca>
2024-04-17 19:12:15 -0400

    Completed Reqs 9-19

--- apod_desktop.py
+++ apod_desktop.py
@@ -255,7 +255,7 @@ def get_apod_info(image_id):
             'title': apod_info[0],
             'explanation': apod_info[1],
             'file_path': apod_info[2],
-            'exists_in_cache': True  # Indicate whether the image exists in the cache
+            'exists_in_cache': True 
         }
     else:
         return None
